80feb85c09b1963c717bf5087ed9e9251489fd91
nouman <nomi676767@gmail.com>
2023-02-04 10:18:22 +0500

    css changes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Serverlogin from "./pages/signin/Serverlogin";
 import Signin from "./pages/signin/Signin";
+import Home from './pages/home/Home';
 
 
 
 
 function App() {
   return (
-    <BrowserRouter>
+		<BrowserRouter>
 			<Routes>
-        <Route path="/signin" element={<Signin></Signin>}></Route>
-        <Route path="/serverlogin" element={<Serverlogin></Serverlogin>}></Route>
+				<Route path="/iocanix" element={<Home></Home>}></Route>
+				<Route path="/iocanix/signin" element={<Signin></Signin>}></Route>
+				<Route
+					path="/iocanix/serverlogin"
+					element={<Serverlogin></Serverlogin>}
+				></Route>
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <center>
+            <div>
+                Home
+            </div>
+        </center>
+    );
+}
+
+export default Home;
